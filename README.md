15f4386af45d76ab18bb6d77c86a148e6bdff039
Azzeddine Ouadyani <azzeddine@tzar.local>
2018-08-10 18:50:17 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # open-source-ideas-twitter-bot
 open-source-ideas-twitter-bot https://github.com/open-source-ideas/open-source-ideas/issues/80
+# Installation
+## Tweepy
+git clone https://github.com/tweepy/tweepy.git
+cd tweepy
+python setup.py install
+## Flask-Hookserver
+pip install Flask-Hookserver
---
+++ app.py
@@ -0,0 +1,43 @@
+import os, sys
+from credentials import *
+from flask import Flask
+from flask_hookserver import Hooks
+import tweepy
+
+# encoding=utf8
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+# flask app
+app = Flask(__name__)
+
+# twitter auth by tweepy
+twitter_auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)
+twitter_auth.set_access_token(twitter_access_token, twitter_access_token_secret)
+twitter_api = tweepy.API(twitter_auth)
+
+# github webhooks & request checking options
+app.config['GITHUB_WEBHOOKS_KEY'] = github_webhooks_secret
+app.config['VALIDATE_IP'] = False
+app.config['VALIDATE_SIGNATURE'] = True
+hooks = Hooks(app, url='/webhooks')
+
+# flask routes
+@app.route('/')
+def hello():
+    return '<a href="{0}">{0}}</a>'.format('https://github.com/open-source-ideas')
+
+@hooks.hook('issues')
+def issues(data, delivery):
+    # Tweet
+    twitter_update_status = 'no-twitter-status-update'
+    tweet = '{0} open source idea! {1} -> {2}'.format(
+        'New' if (data['action'] == 'opened') else 'Updated', 
+        data['issue']['title'], 
+        data['issue']['html_url']
+        )
+    twitter_update_status = twitter_api.update_status(tweet)
+    return str(twitter_update_status)
+    
+# app running on c9.io
+app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
