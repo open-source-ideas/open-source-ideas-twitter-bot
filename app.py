01f6b7602b0f17885664cff534d6d9eebb714b27
azz-eddine <azzeddine.ouadyani@gmail.com>
2018-08-15 19:52:58 +0100

    Tweet only newly created issues

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def issues(data, delivery):
     return str(twitter_update_status)
     
 # app running on c9.io
-app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+# app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
